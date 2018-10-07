e28166d95b9acaf0081f8b21534856e6e40717a3
ryuichi1208 <ryucrosskey@gmail.com>
2018-10-07 16:52:44 +0900

    -m

---
+++ work/scraipe_2.py
@@ -0,0 +1,24 @@
+# coding: UTF-8
+import urllib2
+from bs4 import BeautifulSoup
+
+# アクセスするURL
+url = "http://www.nikkei.com/"
+
+# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
+html = urllib2.urlopen(url)
+
+# htmlをBeautifulSoupで扱う
+soup = BeautifulSoup(html, "html.parser")
+
+# タイトル要素を取得する → <title>経済、株価、ビジネス、政治のニュース:日経電子版</title>
+title_tag = soup.title
+
+# 要素の文字列を取得する → 経済、株価、ビジネス、政治のニュース:日経電子版
+title = title_tag.string
+
+# タイトル要素を出力
+print title_tag
+
+# タイトルを文字列を出力
+print title
